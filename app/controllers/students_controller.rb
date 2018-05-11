deb2991a1fc80b29a515c1b0a997c3a9fe7d6b86
ToroChu <whileonrails@gmail.com>
2018-05-11 22:45:23 +0800

    basic search function implemented

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,16 +1,21 @@
 class StudentsController < ApplicationController
   def index
     @q = Student.ransack(params[:q])
-    @students = @q.result(distinct: true)
+    # @students = @q.result(distinct: true)
+    # solved n+1
+    @students = @q.result.includes(:department, :subjects)
   end
 
   def search
     @q = Student.search(search_params)
-    @students = @q.result(distinct: true)
+    # @students = @q.result(distinct: true)
+    # solved n+1
+    @students = @q.result.includes(:department, :subjects)
   end
 
   private
   def search_params
-    params.require(:q).permit!
+    #params.require(:q).permit!
+    params.require(:q).permit(:name_cont)
   end
 end
